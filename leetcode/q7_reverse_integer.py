68e9623184f25488c3621695c1be7d5d858fe40a
wx <email@example.com>
2018-01-09 18:03:00 +0800

    reverse_integer

--- leetcode/q7_reverse_integer.py
+++ leetcode/q7_reverse_integer.py
@@ -1,2 +1,19 @@
 # -*- coding: utf-8 -*-  
 __author__ = "wangxuan"
+
+
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x < 0:
+            return -self.reverse(-x)
+
+        result = 0
+        while x:
+            result = result * 10 + x % 10
+            x /= 10
+        return result if result <= 0x7fffffff else 0
+
